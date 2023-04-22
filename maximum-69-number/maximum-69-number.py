e0376d5fbeaa5ba1eab20c9820f562177744e5c3
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-04-22 20:58:43 +0900

    Time: 27 ms (87.71%) | Memory: 13.8 MB (91.65%) - LeetSync

--- maximum-69-number/maximum-69-number.py
+++ maximum-69-number/maximum-69-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        num_str_list = list(str(num))
-        for i in range(len(num_str_list)):
-            if num_str_list[i] == "6":
-                num_str_list[i] = "9"
-                return int("".join(num_str_list))
-        return int("".join(num_str_list))
+        num_str = str(num)
+        idx_to_change = num_str.find("6")
+        if idx_to_change == -1:
+            return num
+        else:
+            return num + 3 * 10**(len(num_str)-1-idx_to_change)
