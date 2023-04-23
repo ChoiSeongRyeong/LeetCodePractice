c63cb1abc176ada9079a6d712687491335b8ece5
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-04-23 15:35:56 +0900

    Time: 160 ms (50.29%) | Memory: 14.4 MB (20.39%) - LeetSync

--- maximum-units-on-a-truck/maximum-units-on-a-truck.py
+++ maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -1,12 +1,12 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        sortedByNumberOfUnitsDescended = sorted(
+        boxTypes = sorted(
             boxTypes,
             key=lambda x: x[1],
             reverse=True
         )
         total_units = 0
-        for boxtype in sortedByNumberOfUnitsDescended:
+        for boxtype in boxTypes:
             if truckSize == 0:
                 return total_units
             number_of_boxes_to_put = min(boxtype[0], truckSize)
