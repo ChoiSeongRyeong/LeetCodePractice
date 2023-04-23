818b001d1e493719362a45fdb4dfa98cb91c0169
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-04-23 16:11:05 +0900

    Time: 832 ms (6.10%) | Memory: 44.2 MB (6.10%) - LeetSync

--- largest-subarray-length-k/largest-subarray-length-k.py
+++ largest-subarray-length-k/largest-subarray-length-k.py
@@ -1,6 +1,8 @@
 class Solution:
     def largestSubarray(self, nums: List[int], k: int) -> List[int]:
+        
         import numpy as np
         n = len(nums)
-        max_idx = np.array(nums[:n-k+1]).argmax()
+        
+        max_idx = np.argmax(nums[:n-k+1])
         return nums[max_idx:max_idx+k]
