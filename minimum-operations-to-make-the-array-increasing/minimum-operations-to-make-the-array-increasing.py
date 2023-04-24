dfd20754489b5035a8e8e7d84cff2a9b1bd1dfb3
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-04-24 15:45:45 +0900

    Time: 125 ms (80.17%) | Memory: 14.6 MB (39.04%) - LeetSync

--- minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
+++ minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
@@ -3,6 +3,10 @@ class Solution:
         left_max = nums[0]
         operation_count = 0
         for i in range(1,len(nums)):
-            operation_count += max(0, left_max-nums[i]+1)
-            left_max = max(left_max+1, nums[i])
+            if left_max < nums[i]:
+                left_max = nums[i]
+                continue
+            else:
+                operation_count += left_max-nums[i]+1
+                left_max += 1
         return operation_count
