106630861560048947c0223dc01910f49b43e753
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-04-24 15:47:39 +0900

    Time: 140 ms (22.04%) | Memory: 14.6 MB (83.74%) - LeetSync

--- minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
+++ minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
@@ -1,12 +1,10 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        left_max = nums[0]
         operation_count = 0
         for i in range(1,len(nums)):
-            if left_max < nums[i]:
-                left_max = nums[i]
+            if nums[i-1] < nums[i]:
                 continue
             else:
-                operation_count += left_max-nums[i]+1
-                left_max += 1
+                operation_count += nums[i-1]+1-nums[i]
+                nums[i] = nums[i-1]+1
         return operation_count
