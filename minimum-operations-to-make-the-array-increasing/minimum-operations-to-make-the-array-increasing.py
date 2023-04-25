51641ecb4a0d98a0b582ba42d9548a65c8af56cb
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-04-25 21:35:53 +0900

    Time: 126 ms (77.02%) | Memory: 14.6 MB (84.11%) - LeetSync

--- minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
+++ minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
@@ -1,10 +1,9 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        operation_count = 0
+        count = 0
         for i in range(1,len(nums)):
-            if nums[i-1] < nums[i]:
-                continue
-            else:
-                operation_count += nums[i-1]+1-nums[i]
-                nums[i] = nums[i-1]+1
-        return operation_count
+            if nums[i] <= nums[i-1]:
+                x = nums[i]
+                nums[i] += (nums[i-1] - nums[i]) + 1
+                count += nums[i] - x
+        return count
