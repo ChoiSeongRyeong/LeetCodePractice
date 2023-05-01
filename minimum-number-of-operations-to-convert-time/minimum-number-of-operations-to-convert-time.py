d7656cc23181a033e28f0226cedc347a89444c18
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-05-01 16:18:41 +0900

    Time: 59 ms (5.10%) | Memory: 16.3 MB (6.38%) - LeetSync

--- minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.py
+++ minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.py
@@ -1,10 +1,9 @@
 class Solution:
     def convertTime(self, current: str, correct: str) -> int:
-        def convert_str_to_timestamp(time: str)-> int:
-            return int(time[:2])*60 + int(time[-2:])
-        current_timestamp = convert_str_to_timestamp(current)
-        correct_timestamp = convert_str_to_timestamp(correct)
-        diff = abs(current_timestamp - correct_timestamp)
+        diff = abs(
+            (int(current[:2])*60 + int(current[-2:]))
+            - (int(correct[:2])*60 + int(correct[-2:]))
+        )
         res = 0
         for n in [60,15,5,1]:
             res += diff // n
