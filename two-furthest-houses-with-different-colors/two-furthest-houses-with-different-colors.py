001d267a283f17e9f24cab1bd15f03b98a0913ea
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-05-01 15:53:21 +0900

    Time: 50 ms (31.48%) | Memory: 16.2 MB (5.35%) - LeetSync

--- two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
+++ two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
@@ -3,10 +3,8 @@ class Solution:
         left, right = 0, len(colors)-1
         while colors[left] == colors[right]:
             left += 1
-        max_distance_from_right = right - left
-
-        left, right = 0, len(colors)-1
-        while colors[left] == colors[right]:
-            right -= 1
-        max_distance_from_left = right - left
-        return max(max_distance_from_right, max_distance_from_left)
+            if colors[left] == colors[right]:
+                right -= 1
+            else:
+                break
+        return min(right, len(colors)-1-left)
