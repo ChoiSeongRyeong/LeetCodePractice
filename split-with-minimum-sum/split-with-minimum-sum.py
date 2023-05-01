64dd7c42e18c54d94b46663bed989056ce910908
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-05-01 15:34:42 +0900

    Time: 43 ms (7.13%) | Memory: 16.3 MB (9.48%) - LeetSync

--- split-with-minimum-sum/split-with-minimum-sum.py
+++ split-with-minimum-sum/split-with-minimum-sum.py
@@ -1,4 +1,20 @@
+from queue import PriorityQueue
+
 class Solution:
     def splitNum(self, num: int) -> int:
-        num = ''.join(sorted(str(num)))
-        return sum(map(int, (num[::2], num[1::2])))
+        a = [int(x) for x in str(num)]
+        pq = PriorityQueue()
+        num1 = ""
+        num2 = ""
+        for i in a:
+            pq.put(i)
+
+        while not pq.empty():
+            num1 += str(pq.get())
+            if not pq.empty():
+                num2 += str(pq.get())
+
+        if num2:
+            return int(num1) + int(num2)
+        else:
+            return int(num1)
