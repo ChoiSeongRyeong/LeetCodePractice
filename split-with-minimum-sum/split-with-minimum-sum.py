2d9ef6734e77bb95c04ad69280830d38494986ac
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-05-01 15:38:54 +0900

    Time: 55 ms (7.13%) | Memory: 16.3 MB (9.48%) - LeetSync

--- split-with-minimum-sum/split-with-minimum-sum.py
+++ split-with-minimum-sum/split-with-minimum-sum.py
@@ -1,14 +1,5 @@
 class Solution:
     def splitNum(self, num: int) -> int:
-        hp = [int(i) for i in str(num)]
-        heapify(hp)
-        
-        n1 = ''
-        n2 = ''
-        
-        while hp:
-            n1 += str(heappop(hp))
-            if hp:
-                n2 += str(heappop(hp))
-        
-        return int(n1) + int(n2)
+        digits = sorted(str(num))
+        return (int("".join(d for d in digits[::2])) +
+                int("".join(d for d in digits[1::2])))
