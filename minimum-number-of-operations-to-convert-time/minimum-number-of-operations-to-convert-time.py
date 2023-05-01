2bee77657bba69308d6432a09c763430d972a210
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-05-01 16:20:13 +0900

    Time: 48 ms (7.91%) | Memory: 16.2 MB (7.65%) - LeetSync

--- minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.py
+++ minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.py
@@ -1,8 +1,8 @@
 class Solution:
     def convertTime(self, current: str, correct: str) -> int:
-        diff = abs(
-            (int(current[:2])*60 + int(current[-2:]))
-            - (int(correct[:2])*60 + int(correct[-2:]))
+        diff = (
+            (int(correct[:2])*60 + int(correct[-2:]))
+            -(int(current[:2])*60 + int(current[-2:])) 
         )
         res = 0
         for n in [60,15,5,1]:
