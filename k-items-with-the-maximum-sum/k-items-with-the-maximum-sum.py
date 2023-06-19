0a72956ff0db3438cb2168396e008d72bdd0f38b
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-06-19 20:33:49 +0900

    Time: 49 ms (61.85%) | Memory: 16.4 MB (27.50%) - LeetSync

--- k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.py
+++ k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
-        if k > numOnes:
-            if k > numOnes + numZeros:
-                return numOnes - (k - numOnes - numZeros)
-            else:
-                return numOnes
-        else:
+        if k <= numOnes:
             return k
+        elif k <= numOnes + numZeros:
+            return numOnes
+        else:
+            return 2*numOnes + numZeros - k
