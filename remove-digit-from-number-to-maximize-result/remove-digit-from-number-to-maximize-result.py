f21b7f8114dba124d553545dbc2ef3e2b9aec8b0
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-07-03 21:45:23 +0900

    Time: 48 ms (59.83%) | Memory: 16.3 MB (28.89%) - LeetSync

--- remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.py
+++ remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.py
@@ -1,17 +1,13 @@
 class Solution:
     def removeDigit(self, number: str, digit: str) -> str:
         i = 0
-        while i < len(number)-1:
+        for i in range(len(number)-1):
             if number[i] == digit:
                 if number[i] < number[i+1]:
                     return number[:i]+number[i+1:]
-                else:
-                    target_idx = i
-                    i += 1
-            else:
-                i += 1
+                target_idx = i
 
         if number[-1] == digit:
-            target_idx = i
+            target_idx = i+1
 
         return number[:target_idx]+number[target_idx+1:]
