c565c69224236ee45ff7ae2e49268adb2443ddca
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-07-08 11:59:09 +0900

    Time: 48 ms (64.72%) | Memory: 16.3 MB (73.90%) - LeetSync

--- minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.py
+++ minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.py
@@ -4,9 +4,8 @@ class Solution:
         gap = amount[2]-amount[1]
 
         if amount[0] <= gap:
-            ans = amount[0] + max(amount[2] - amount[0], amount[1])
+            return amount[0] + max(amount[2] - amount[0], amount[1])
         elif (amount[0] - gap) % 2 == 0:
-            ans = (amount[0] - gap) // 2 + amount[2]
+            return (amount[0] - gap) // 2 + amount[2]
         else:
-            ans = (amount[0] - gap) // 2 + 1 + amount[2]
-        return ans
+            return (amount[0] - gap) // 2 + 1 + amount[2]
