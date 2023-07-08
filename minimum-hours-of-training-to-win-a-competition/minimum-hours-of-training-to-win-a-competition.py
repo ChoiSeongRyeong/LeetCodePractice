362e4816aff1138104dd6a7132802b094695b4a4
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-07-08 12:21:46 +0900

    Time: 69 ms (8.33%) | Memory: 16.4 MB (47.78%) - LeetSync

--- minimum-hours-of-training-to-win-a-competition/minimum-hours-of-training-to-win-a-competition.py
+++ minimum-hours-of-training-to-win-a-competition/minimum-hours-of-training-to-win-a-competition.py
@@ -2,12 +2,11 @@ class Solution:
     def minNumberOfHours(self, initialEnergy: int, initialExperience: int, energy: List[int], experience: List[int]) -> int:
         expected_ene = sum(energy)+1
 
-        expected_exps = []
+        expected_exp = 1
         exp_cum = 0
         for exp in experience:
-            expected_exps.append(exp-exp_cum)
+            expected_exp = max(expected_exp, (exp-exp_cum)+1)
             exp_cum += exp
-        expected_exp = max(expected_exps)+1
 
         return(
             max(0, expected_ene-initialEnergy)
