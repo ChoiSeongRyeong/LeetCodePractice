aef2f9252d8c7aa71964b7feb67dd460b3777306
ChoiSeongRyeong <77564556+ChoiSeongRyeong@users.noreply.github.com>
2023-07-08 12:05:52 +0900

    Time: 42 ms (87.47%) | Memory: 16.1 MB (95.62%) - LeetSync

--- minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.py
+++ minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.py
@@ -2,8 +2,8 @@ class Solution:
     def fillCups(self, amount: List[int]) -> int:
         amount.sort()
         gap = amount[2]-amount[1]
-
-        if amount[0] <= gap:
-            return amount[0] + max(amount[2] - amount[0], amount[1])
+        key = amount[0] + amount[1] - amount[2]
+        if key <= 0:
+            return amount[2]
         else:
-            return (amount[0] - gap) // 2 + (amount[0] - gap) % 2 + amount[2]
+            return ceil(key/2) + amount[2]
